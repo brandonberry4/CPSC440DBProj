c9f424ad94e1e2faecdceca7be397ac0b7b4789c
Brandon <brandon.berry.17@cnu.edu>
2020-12-01 20:08:22 -0500

    added signup page

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,10 @@
-<html>    
-  ￼  <head>    
-        <title>Registration Form</title>    
+<html>
+    <head>    
+        <title>Sign-up</title>    
     </head>    
     <body>    
         <link href = "style.css" type = "text/css" rel = "stylesheet" />    
-        <h2>Sign In</h2>    
+        <h2>Sign-Up</h2>    
         <form name = "form1" action="modified.php" method = "post" enctype = "multipart/form-data" >    
             <div class = "container">    
                 <div class = "form_group">    
@@ -18,7 +18,20 @@
                 <div class = "form_group">    
                     <label>ID:</label>    
                     <input type = "text" name = "lname" value = "" required/>    
-                </div>     
+                </div>
+				<label for="Sport">Sport:</label>
+
+				<select> name="sport" id="sports">
+				<option value="Empty">-</option>
+				<option value="Track & Field">Track & Field</option>
+				<option value="Football">Football</option>
+				<option value="Soccer">Soccer</option>
+				<option value="Basketball">Basketball</option>
+				</select>
+				
+				<form action="signin.html">
+				<input type="submit" value="Go to Sign-in Page" />
+				</form>
             </div>    
         </form>    
     </body>    
